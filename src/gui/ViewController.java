f69bf4d59ea91f0b875e9f37205a2b32af780066
vitoria <vitoriabccorrea@gmail.com>
2022-07-19 18:54:35 -0300

    contraints e initializable

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,13 +1,17 @@
 package gui;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
+import gui.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class ViewController {
+public class ViewController implements Initializable{
 	@FXML
 	private Button btMyButton;
 	@FXML
@@ -24,5 +28,13 @@ public class ViewController {
 		double number2=Double.parseDouble(txtNumber2.getText());
 		double sum=number1+number2;
 		labelResult.setText(String.format("%.2f", sum));	
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldMaxLength(txtNumber1, 14);
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber2, 14);
 	}
 }
